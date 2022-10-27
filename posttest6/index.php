bee643face1935899adb26cd0948dda77bf48967
Steven Alesandro nesta <113688134+Steven090902N@users.noreply.github.com>
2022-10-27 23:20:41 +0800

    commit 1

---
+++ posttest6/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Steven090902N</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+
+<body>
+    <div class="hero">
+        <div class="navbar">
+            <img src="show.png" class="logo">
+        </div>
+        <div class="content">
+            <h2>WELCOME TO<br><span> SHOWROOM NESTA</span></h2>
+            <a href="index1.php"><button type="button" ><b>EXPLORE</b></button></a>
+        </div>
+    </div>
+</body>
+
+</html>
